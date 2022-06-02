8f1ca9a73cbe86c062bfaf9671eea3d561b3777c
Bennett Kautz <bennettkautz@gmail.com>
2022-06-02 14:11:55 -0400

    tests

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,8 +1,12 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import Server from "webpack-dev-server";
+import React from "react";
+import Spinner from './Spinner'
+import { render, screen, fireEvent } from "@testing-library/react";
+
+
+test('Spinner is not rendering', async() => {
+  render(<Spinner/>)
+ 
 })
-
-
-
